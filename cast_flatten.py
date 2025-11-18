1a24e958197c914809ffcac6931a13b44b2ad485
HighCloud <highcloud333@outlook.com>
2025-11-18 17:33:32 +0800

    fix

--- cast_flatten.py
+++ cast_flatten.py
@@ -14,7 +14,7 @@ def cast_kernel(
     pid = tl.program_id(0)  # 程序 ID
     
     blk_beg = pid * BLOCK_Y
-    X1 = tl.arange(0, BLOCK_X)
+    x1 = tl.arange(0, BLOCK_X)
     y0 = blk_beg + tl.arange(0, BLOCK_Y)
     idx = x1[None, :] + y0[:, None] * BLOCK_X
     tmp0 = tl.load(input_ptr + idx)
@@ -23,7 +23,7 @@ def cast_kernel(
 
 
 def triton_func(x0: torch.Tensor) -> torch.Tensor:
-    output = torch.empty_like(x0)
+    output = torch.empty_like(x0, dtype=torch.float16)
     grid = lambda meta: (triton.cdiv(x0.shape[0], meta['BLOCK_Y']),)
     cast_kernel[grid](
         x0,
@@ -41,11 +41,11 @@ def torch_func(x0: torch.Tensor) -> torch.Tensor:
 if __name__ == "__main__":
     torch.manual_seed(0)
     size = (16, 32)
-    x0 = torch.randn(size, device="npu", dtype=torch.float32)
-    output_triton = triton_func(x0)
-    output_torch = torch_func(x0)
+    x = torch.randn(size, device="npu", dtype=torch.float32)
+    output_triton = triton_func(x)
+    output_torch = torch_func(x)
     # 验证结果
-    print(f"输入形状: x={x.shape}, y={y.shape}")
+    print(f"输入形状: x={x.shape}")
     print(f"输出形状: {output_triton.shape}")
     print(f"最大误差: {torch.max(torch.abs(output_triton - output_torch)).item():.6f}")
     print(f"是否相等: {torch.allclose(output_triton, output_torch)}")
